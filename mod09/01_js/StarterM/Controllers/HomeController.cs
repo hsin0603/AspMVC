7aa339095fcf225e75b4a7947f31cca147545479
Hsin <gemini880603@gmail.com>
2022-02-24 16:48:24 +0800

    'Mod09'

--- mod09/01_js/StarterM/Controllers/HomeController.cs
+++ mod09/01_js/StarterM/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StarterM.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,6 +26,36 @@ namespace StarterM.Controllers
         public IActionResult FormDemo()
         {
             return View();
+        }
+
+        public IActionResult CSSDemo()
+        {
+            return View();
+        }
+
+        public IActionResult AjaxDemo()
+        {
+            return View();
+        }
+
+        // /home/getbook/1
+        // /home/getbook?id=2
+        public IActionResult GetBook(int id)
+        {
+            List<Book> books = new List<Book> {
+                new Book { BookId=1, Title="C#", Price=1000 },
+                new Book { BookId=2, Title="VB", Price=500 },
+                new Book { BookId=3, Title="JS", Price=1200 },
+                new Book { BookId=4, Title="MVC", Price=1500 },
+                new Book { BookId=5, Title="XML", Price=300 }
+            };
+            var book = books.Find(b => b.BookId == id);
+            if (book==null)
+            {
+                return NotFound();
+            }
+
+            return Json(book);
         }
     }
 }
---
+++ mod09/01_js/StarterM/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarterM.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public int Price { get; set; }
+
+    }
+}
